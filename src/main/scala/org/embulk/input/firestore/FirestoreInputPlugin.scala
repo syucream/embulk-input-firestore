48b356d87295d6941447f3677d1653f4c5bee856
Ryo Okubo <syucream1031@gmail.com>
2020-01-01 00:56:50 +0900

    cleanup

--- src/main/scala/org/embulk/input/firestore/FirestoreInputPlugin.scala
+++ src/main/scala/org/embulk/input/firestore/FirestoreInputPlugin.scala
@@ -3,16 +3,25 @@ package org.embulk.input.firestore
 import java.util
 
 import org.embulk.config.{ConfigDiff, ConfigSource, TaskReport, TaskSource}
-import org.embulk.spi.{InputPlugin, PageOutput, Schema}
+import org.embulk.spi.{Exec, InputPlugin, PageOutput, Schema}
 
 case class FirestoreInputPlugin() extends InputPlugin {
-  override def transaction(config: ConfigSource, control: InputPlugin.Control): ConfigDiff = ???
 
-  override def resume(taskSource: TaskSource, schema: Schema, taskCount: Int, control: InputPlugin.Control): ConfigDiff = ???
+  override def transaction(config: ConfigSource, control: InputPlugin.Control): ConfigDiff = {
+    null
+  }
 
-  override def cleanup(taskSource: TaskSource, schema: Schema, taskCount: Int, successTaskReports: util.List[TaskReport]): Unit = ???
+  override def resume(taskSource: TaskSource, schema: Schema, taskCount: Int, control: InputPlugin.Control): ConfigDiff = {
+    null
+  }
 
-  override def run(taskSource: TaskSource, schema: Schema, taskIndex: Int, output: PageOutput): TaskReport = ???
+  override def cleanup(taskSource: TaskSource, schema: Schema, taskCount: Int, successTaskReports: util.List[TaskReport]): Unit = {
+  }
 
-  override def guess(config: ConfigSource): ConfigDiff = ???
+  override def run(taskSource: TaskSource, schema: Schema, taskIndex: Int, output: PageOutput): TaskReport = {
+    null
+  }
+
+  override def guess(config: ConfigSource): ConfigDiff =
+    Exec.newConfigDiff()
 }
